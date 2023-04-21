b0e2aa06f6062188e1f52eb8e1225e9a9ed65569
Junx Yang jyan3795 <111340306+Tanheishanyang@users.noreply.github.com>
2023-04-21 18:19:03 +1000

    Add files via upload

---
+++ INFO1111_self_learning.cs
@@ -0,0 +1,32 @@
+﻿//This is a program I haven wirte
+//it  could check weather or not the world's length is between the 0 to 20
+using System;
+
+namespace WordLengthChecker
+{
+    class Program
+    //Define object types
+    {
+        static void Main(string[] args)
+            //starting point
+        {
+            string myWord = "hello_world";
+            bool isLengthValid = CheckWordLength(myWord);//call a function
+            Console.WriteLine("The length of the word '" + myWord + "' is valid: " + isLengthValid);
+            Console.ReadKey();
+        }
+        //define a function
+        public static bool CheckWordLength(string word)
+        {
+            int length = word.Length;
+            if (length >= 0 && length <= 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
